23aa1d90f13183b77621b49fbbc4cf7cd35c94ef
taylorjames9 <taylorjames9@gmail.com>
2015-10-20 16:41:40 -0700

    NEW SOUND FX for shoot

--- Assets/Scripts/Tutorial_Script.cs
+++ Assets/Scripts/Tutorial_Script.cs
@@ -12,7 +12,7 @@ public class Tutorial_Script : MonoBehaviour {
 
 	void Awake(){
 		//add all strings with tutorial to list
-    myTutorialTextSnippets.Add ("This is the first tutorial tile.");
+    myTutorialTextSnippets.Add ("This is the first tutorial tile. This is the first tutorial tile. This is the first tutorial tile. This is the first tutorial tile. This is the first tutorial tile. This is the first tutorial tile. This is the first tutorial tile. This is the first tutorial tile. This is the first tutorial tile. This is the first tutorial tile. ");
     myTutorialTextSnippets.Add ("This is the Second tutorial tile.");
     myTutorialTextSnippets.Add ("This is the third tutorial tile.");
     myTutorialTextSnippets.Add ("This is the fourth tutorial tile.");
@@ -29,8 +29,24 @@ public class Tutorial_Script : MonoBehaviour {
   }
 
 	public void FlipOneThroughTutorial(string dir){
+    Debug.Log ("Flipping Through");
     int i = myTutorialTextSnippets.IndexOf (tutTextDisplayArea.text);
-    tutTextDisplayArea.text = myTutorialTextSnippets [dir == "next" ? i++ : i--];
+    if (dir == "next") {
+      if(i < myTutorialTextSnippets.Count -1){
+        Debug.Log ("NEXT WE GO");
+        i++;
+      } else {
+        i=0;
+      }
+    } else if (dir == "back"){
+      if(i > 0){
+        Debug.Log ("BACK WE GO");
+        i--;
+      } else {
+        i = myTutorialTextSnippets.Count -1;
+      }
+    }
+    tutTextDisplayArea.text = myTutorialTextSnippets [i];
   }
 
 
